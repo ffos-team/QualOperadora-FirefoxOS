c8d1d4e2ad6abac7dbe940fd56f211cd161a4c59
Daniel Röhers Moura <rohersmoura@gmail.com>
2014-09-09 11:40:00 -0300

    refactor gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,36 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    minifyCSS = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var zip = require('gulp-zip');
 
 gulp.task('minifyJS', function () {
-  gulp.src('assets/js/app.js')
+  return gulp.src('assets/js/*.js')
     .pipe(uglify())
-    .pipe(rename('app.min.js'))
-    .pipe(gulp.dest('assets/js/'))
+    .pipe(rename('*.js'))
+    .pipe(gulp.dest('dist/assets/js/'))
 });
 
 gulp.task('minifyCSS', function() {
-  gulp.src('assets/css/style.css')
+  return gulp.src('assets/css/*.css')
     .pipe(minifyCSS({keepBreaks: false}))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('assets/css'))
+    .pipe(rename('*.css'))
+    .pipe(gulp.dest('dist/assets/css'))
+});
+
+gulp.task('images', function () {
+	return gulp.src('assets/img/*')
+		.pipe(gulp.dest('dist/assets/img'))
+});
+
+gulp.task('moving-dist', function () {
+	return gulp.src(['*.html', '*.webapp'])
+		.pipe(gulp.dest('dist/'));
 });
 
 // $ gulp deploy
-gulp.task('deploy', ['minifyJS', 'minifyCSS']);
+gulp.task('deploy', ['minifyJS', 'minifyCSS', 'images', 'moving-dist'], function () {
+	gulp.src('dist/*')
+    .pipe(zip('qualoperadora.zip'))
+    .pipe(gulp.dest('dist/'));
+});
